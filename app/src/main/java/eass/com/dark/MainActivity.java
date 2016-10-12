aaf47596b7a16d674578dcc117e9307a4212937f
easseptember <15522280@qq.com>
2016-10-12 21:52:15 +0800

    保存密码功能

--- app/src/main/java/eass/com/dark/MainActivity.java
+++ app/src/main/java/eass/com/dark/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.*;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,6 +22,18 @@ public class MainActivity extends AppCompatActivity {
         final EditText myPhone      =  (EditText)   findViewById(R.id.phone);
         final EditText password     =  (EditText)   findViewById(R.id.password);
         Button   mySubmit     =  (Button)     findViewById(R.id.mysubmit);
+
+        /**
+         * ============================================================================
+         * 日志级别
+         * ============================================================================
+         */
+        Log.i("main", "info");
+        Log.e("main", "error");
+        Log.d("main", "debug");
+
+
+
 /**
  * ============================================================================
  * 监听器
@@ -30,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
                 String phoneNumber  = myPhone.getText().toString().trim();
                 String myPassword     = password.getText().toString().trim();
 
-                if("".equals(phoneNumber) || "".equals(myPassword)){
+                //if("".equals(phoneNumber) || "".equals(myPassword)){  //判断空  方法1
+                if(TextUtils.isEmpty(phoneNumber) || TextUtils.isEmpty(myPassword)){//判断空  方法2
                     Toast.makeText(MainActivity.this, "username or password can't be empty!",Toast.LENGTH_LONG).show();//tips
                 }else if("admin".equals(phoneNumber) && "123456".equals(myPassword)){
                     //*拨打电话*//
@@ -43,11 +59,26 @@ public class MainActivity extends AppCompatActivity {
                     //开启意图
                     startActivity(intent);
                     */
+                    CheckBox checkSaveInfo = (CheckBox) findViewById(R.id.checkSaveInfo);
+                    if(checkSaveInfo.isChecked()){
+                        boolean result = Utils.saveInfos(phoneNumber, myPassword);
+                        if(result){
+                            Intent intent2 = new Intent();
+                            intent2.setClass(MainActivity.this, My.class);//方法2  跳转
+                            startActivity(intent2);
+                            System.out.println("it's click" + phoneNumber + myPassword);
+                        }else{
+                            Toast.makeText(MainActivity.this, "save failed!",Toast.LENGTH_SHORT).show();
+                        }
 
-                    Intent intent2 = new Intent();
-                    intent2.setClass(MainActivity.this, My.class);//方法2
-                    startActivity(intent2);
-                    System.out.println("it's click" + phoneNumber + myPassword);
+
+                    }else{
+                        Toast.makeText(MainActivity.this, "not checked!",Toast.LENGTH_SHORT).show();
+                    }
+
+                    /*
+
+                    */
 
                 }
 
---
+++ app/src/main/java/eass/com/dark/Utils.java
@@ -0,0 +1,35 @@
+package eass.com.dark;
+
+import java.io.*;
+
+/**
+ * @author Eass September<15522280@qq.com>
+ * @date   201610122127
+ * @description 工具类
+ */
+
+public class Utils {
+    /**
+     *
+     * @param   userName
+     * @param   password
+     * @return  boolean
+     */
+    public static boolean saveInfos(String userName, String password){
+        try {
+            String  info = userName + "##" + password;
+            File    file = new File("data/data/eass.com.dark/info.txt");
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(info.getBytes());
+            fos.close();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+
+
+    }
+
+
+}
