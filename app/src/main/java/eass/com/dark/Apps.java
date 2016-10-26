9c76e03e2ad635d0c1ef339bae6ae59038ec79c2
easseptember <15522280@qq.com>
2016-10-26 21:41:57 +0800

    SqliteDb

--- app/src/main/java/eass/com/dark/Apps.java
+++ app/src/main/java/eass/com/dark/Apps.java
@@ -1,5 +1,7 @@
 package eass.com.dark;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,19 +10,47 @@ import android.support.v7.widget.ViewStubCompat;
 import android.view.View;
 import android.widget.Toast;
 
+import eass.com.sqlite.SqliteDb;
+
 /**
  * Created by Eass_September on 2016/10/24.
  */
 
 public class Apps extends AppCompatActivity {
+
+    private SqliteDb sqliteDb;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.apps);
 
+        //数据库
+        sqliteDb = new SqliteDb(getApplicationContext());
+        //打开或者创建数据库  第一次打开
+        //SQLiteDatabase sqlitedatabase = sqliteDb.getWritableDatabase();
+        //同上 如果磁盘满了  返回只读的
+        //SQLiteDatabase sqlitereadbledatabase = sqliteDb.getReadableDatabase();
+
+
     }
 
     public void showSome(View v){
-        Toast.makeText(this, "apps", Toast.LENGTH_SHORT).show();
+        SQLiteDatabase db = sqliteDb.getWritableDatabase();
+        db.execSQL("insert into info(name,password) values (?,?)", new Object[]{"eass", "123456"});//增加 修改 删除共用
+        //查询
+
+        Cursor cursor = db.rawQuery("select * from info", null);//查找
+        if(cursor != null && cursor.getCount()>0){
+            while (cursor.moveToNext()){
+                String name = cursor.getString(1);
+                String password = cursor.getString(2);
+                System.out.println("name:"+name+"------password:"+password);
+            }
+        }
+        db.close();
+
+
+
+        //Toast.makeText(this, "apps", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/eass/com/sqlite/SqliteDb.java
@@ -0,0 +1,38 @@
+package eass.com.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Eass_September on 2016/10/26.
+ */
+
+public class SqliteDb extends SQLiteOpenHelper {
+
+    public SqliteDb(Context context) {
+        super(context, "dark.db", null, 4);
+    }
+
+    /**
+     * 第一次创建时候的调用  适合表结构的初始化
+     * @param db desc
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table info(id integer primary key autoincrement,name varchar(20))";
+        db.execSQL(sql);
+    }
+
+    /**
+     * 适合更新表结构   在数据更新时调用
+     * @param db
+     * @param oldVersion
+     * @param newVersion
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "alter table info add password varchar(20)";
+        db.execSQL(sql);
+    }
+}
