cb172ff8ac28fbcac7704e5a597f083006fe9a03
easseptember <15522280@qq.com>
2016-10-27 21:44:36 +0800

    数据库

--- app/src/main/java/eass/com/dark/Apps.java
+++ app/src/main/java/eass/com/dark/Apps.java
@@ -1,5 +1,6 @@
 package eass.com.dark;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -36,6 +37,13 @@ public class Apps extends AppCompatActivity {
 
     public void showSome(View v){
         SQLiteDatabase db = sqliteDb.getWritableDatabase();
+
+        /**
+         * ============================================================================
+         * //数据库操作
+         * ============================================================================
+         */
+        /*
         db.execSQL("insert into info(name,password) values (?,?)", new Object[]{"eass", "123456"});//增加 修改 删除共用
         //查询
 
@@ -48,9 +56,57 @@ public class Apps extends AppCompatActivity {
             }
         }
         db.close();
+        */
+
+
+
+        /**
+         * ============================================================================
+         *  GOOGLE API 数据库操作
+         * ============================================================================
+         */
+        //---------------insert----------------//
+        /*
+        ContentValues values = new ContentValues();
+        values.put("name", "张三");
+        values.put("password", "32566678");
+        long info = db.insert("info", null, values);
+        if(info > 0){
+            Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT).show();
+        }
+        */
+        //---------------delete----------------//
+        /*
+        int info = db.delete("info", "id=?", new String[]{"8"});//返回值是影响的行数
+        Toast.makeText(this, "删除的行数是"+info, Toast.LENGTH_SHORT).show();
+        */
+        //---------------update----------------//
+        /*
+        ContentValues values = new ContentValues();
+        values.put("name", "wzq");
+        values.put("password", "wangzhiqiang");
+        int info = db.update("info", values, "id=?", new String[]{"10"});
+        Toast.makeText(this, "更新的行数是"+info, Toast.LENGTH_SHORT).show();
+        */
+        //---------------select----------------//
+        Cursor cursor = db.query("info", new String[]{"password"}, "id=?", new String[]{"9"},null, null, null);
+
+        if(cursor != null && cursor.getCount()>0){
+            while (cursor.moveToNext()){
+                String password = cursor.getString(0);
+
+                System.out.println("password:"+password);
+            }
+        }
+        db.close();
 
 
 
         //Toast.makeText(this, "apps", Toast.LENGTH_SHORT).show();
     }
+
+
+
 }
